6134c6bfcb0953f6cab5a614f126cbd10730b507
vinod v <valajaramachandranmuniraj@gmail.com>
2025-08-30 12:09:33 +0530

    changes

---
+++ music-list.js
@@ -0,0 +1,123 @@
+let allMusic = [
+    {
+        name: "Samajavaragamana",
+        artist: "Sid Sriram",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "Jamal Kudu",
+        artist: "Harshavardan Rameshwar",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Samayama",
+        artist: "Anurag Kulkarni,Sithara Krishnakumar",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "Em Cheppanu",
+        artist: "karthik",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "Tujhe Dekha To",
+        artist: "Lata Mangeshkar,Kumar Sanu",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "Shape-of-You",
+        artist: "Ed Sheeran",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Calm-Down",
+        artist: "Rema, Selena Gomez",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Let Me Down Slowly",
+        artist: "Alec Benjamin",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Love Me Like You Do",
+        artist: "Ellie Goulding",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Starboy",
+        artist: "The Weeknd",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Padi Padi Leche ",
+        artist: "Armaan Malik,Sinduri Vishal",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Arere Yekkada",
+        artist: "Naresh Iyer,Manisha Eerabathini",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Manike",
+        artist: "Yohani,Jublin Nautiyal",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Sugar and Brownies",
+        artist: "Dharia",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Pareshanura",
+        artist: "Padmalatha,Vishnupriya Ravi",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "Baby Wont You Tell Me",
+        artist: "Swetha Mohan,Siddharth Mahadevan",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "Nee Chuttu Chuttu",
+        artist: "Thaman S,Sid Sriram",
+        img: "music-17",
+        src: "music-17"
+    },
+    {
+        name: "Priya Mithunam",
+        artist: "Ajay-Atul,Karthik,Shweta Mohan",
+        img: "music-18",
+        src: "music-18"
+    },
+    {
+        name: "Tere Vaaste",
+        artist: "Varun Jain,Sachin-Jigar,Shadab Faridi",
+        img: "music-19",
+        src: "music-19"
+    },
+    {
+        name: "Tum Tum",
+        artist: "Thaman S,Srivardhani Kuchi",
+        img: "music-20",
+        src: "music-20"
+    },
+
+];
